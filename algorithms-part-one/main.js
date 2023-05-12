1fd65d3218a1ad79fcf6a5e91e904df614019a58
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-05-12 15:29:56 -0300

    Finish first activity

--- algorithms-part-one/main.js
+++ algorithms-part-one/main.js
@@ -1,13 +1,14 @@
 /* Algoritmos complementarios. Parte I */
 
-/* Ejercicio 1. Homero */ 
+/* Ejercicio 1. Homero */  
 
 let nombre = prompt("Bienvenido. Ingrese su nombre por favor"); 
 let apellido = prompt("Excelente. Ahora, ingrese su apellido"); 
 let edad = parseInt(prompt("Por ultimo, ingrese su edad"));
 alert(`Hola ${nombre} ${apellido}. Su edad es ${edad}`);
 
-/* Ejercicio 2. Ciudades */ 
+
+/* Ejercicio 2. Ciudades */  
 
 const PRIMERACIUDAD = "Springfield";
 const SEGUNDACIUDAD = "Miami";
@@ -15,10 +16,11 @@ const TERCERACIUDAD = "New York";
 const CUARTACIUDAD = "Los Angeles";
 const QUINTACIUDAD = "Minessotta";
 
+
 alert(`El nombre de la primer ciudad es ${PRIMERACIUDAD}, el de la segunda es ${SEGUNDACIUDAD}, el de la tercera es ${TERCERACIUDAD}, el de la cuarta es ${CUARTACIUDAD} y el de la quinta es ${QUINTACIUDAD}`);
 
 
-/* Ejercicio 3. Carnet */
+/* Ejercicio 3. Carnet */ 
 
 let nombreConductor = prompt("Bienvenido. Vamos a generar su carnet de conductor. Ingrese su nombre por favor");
 let apellidoConductor = prompt("Excelente. Ahora, ingrese su apellido");
@@ -36,6 +38,25 @@ let carnet = nombreConductor + " " + apellidoConductor + " " + nacionalidad + " 
 alert(carnet);
 
 
+/* Ejercicio 4. Registro de personas */ 
 
+alert(`Bienvenido al sistema de registro de personas. Presione enter para continuar.`);
+let primerIntegrante = prompt("Ingrese el nombre del primer integrante de su familia (preferentemente usted)");
+let segundoIntegrante = prompt("Ahora, ingrese el nombre del segundo integrante de su familia (preferentemente su pareja)");
+let tercerIntegrante = prompt("Ingrese el nombre del tercer integrante de su familia (preferentemente su primer hijo)");
+let cuartoIntegrante = prompt("Ingrese el nombre del quarto integrante de su familia (preferentemente su segundo hijo)");
+let quintoIntegrante = prompt("Ingrese el nombre del quinto integrante de su familia (tercer hijo, tio, primo, sobrino, ahijado)");
+let integrantes = "Primer integrante: " + primerIntegrante + " Segundo integrante:" + segundoIntegrante + " Tercer integrante:" + tercerIntegrante + " Cuarto integrante:" + cuartoIntegrante + " Quinto integrante:" + quintoIntegrante;
+alert(integrantes);
+
+
+/* Ejercicio 5. Descuentos */ 
+
+alert(`Bienvenido a la calculadora de descuentos. Presione enter para continuar.`);
+let valor = prompt("Ingrese el precio del producto que quiere calcularle el descuento");
+let descuento = prompt("Ahora, ingrese el descuento (sin simbolo %) que desea realizarle al producto. Por ejemplo, 20.");
+descuento = (valor * descuento) / 100;
+valor = valor - descuento;
+alert(`Excelente. Usted obtiene ${descuento} pesos de descuento y su producto pasa a tener un valor final de ${valor} pesos. Le aconsejamos realizar la compra!`);
 
 
